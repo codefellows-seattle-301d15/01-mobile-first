240864fb6b73a7fbf100de49faa430cce989eaba
Rick Patci <rpatci@gmail.com>
2016-05-09 00:35:28 -0700

    add starter-code class-01

---
+++ starter-code/scripts/article.js
@@ -0,0 +1,25 @@
+var articles = [];
+
+function Article (opts) {
+  this.author = opts.author;
+}
+
+Article.prototype.toHtml = function() {
+  var $newArticle = $('article.template').clone();
+  $newArticle.attr('data-category', this.category);
+  $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+  $newArticle.append('<hr>');
+  return $newArticle;
+};
+
+ourLocalData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+ourLocalData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
+
+articles.forEach(function(a){
+  $('#articles').append(a.toHtml());
+});
